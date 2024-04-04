6d19fdcbbf31c7ef3af1e978a88d3d4f76186fb9
Kobe Pho <kobeypho@gmail.com>
2024-04-04 13:36:44 -0700

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
